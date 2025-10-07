09da5f640dcba952b162454b2331aa21e7811d4e
Samir-ibourki <samiribourki7@gmail.com>
2025-10-07 16:50:28 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import './index.css'
 import React from "react";
 import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
 import Header from './component/header'
-
+import SideBar from './component/sidebar'
 function App() {
   return (
    <>
    <Header/>
+<SideBar/>
    </>
   )
 }
---
+++ src/component/sidebar.jsx
@@ -0,0 +1,30 @@
+
+export default function SideBar() {
+    return (
+        <>
+
+        <section className="bg-[#969737] h-[100vh] w-[20%] decoration-none">
+
+            <div className="flex flex-col gap-[1rem] items-center ">
+              <a className="bg-[#eee] w-[90%] hover:bg-[black] rounded-[0.4rem] p-[0.4rem] text-center mt-[1rem] cursor-pointer text-[black] hover:text-[white] no-underline" href="">shop</a>
+              <a className="bg-[#eee] w-[90%] hover:bg-[black] rounded-[0.4rem] p-[0.4rem] text-center mt-[1rem] cursor-pointer text-[black] hover:text-[white] no-underline" href="">checkout</a>
+              <a className="bg-[#eee] w-[90%] hover:bg-[black] rounded-[0.4rem] p-[0.4rem] text-center mt-[1rem] cursor-pointer text-[black] hover:text-[white] no-underline" href="">add to cart</a>
+                
+                
+                
+            </div>
+
+</section>
+        </>
+
+
+    )
+}
+
+
+
+
+
+
+
+
